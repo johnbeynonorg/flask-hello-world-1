2a8e26fed6dc95630c3b09d70cae192b40370449
John Beynon <john@thebeynons.uk>
2022-09-02 11:09:45 +0100

    Update app.py

--- app.py
+++ app.py
@@ -4,3 +4,16 @@ app = Flask(__name__)
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.before_request
+def gather_request_data():
+    g.method = request.method
+    g.url = request.url
+
+@app.after_request
+def log_details(response: Response):
+    g.status = response.status
+
+    logger.info(f'method: {g.method}\n url: {g.url}\n status: {g.status}')
+
+    return response
